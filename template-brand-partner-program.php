f4533c7813faf437444137354d660dee39b11496
Justin Estrada <6895474+justinestrada@users.noreply.github.com>
2026-06-25 19:50:07 -0600

    Brand Partner Program Content (#3)
    * Brand Partner Program Content
    
    * Brand Partner Hero Image

--- template-brand-partner-program.php
+++ template-brand-partner-program.php
@@ -5,17 +5,8 @@
 
 get_header(); ?>
 
-<main>
-  <div class="container py-5">
-    <?php while (have_posts()) : the_post(); ?>
-      <article <?php post_class(); ?>>
-        <h1><?php the_title(); ?></h1>
-        <div class="entry-content">
-          <?php get_template_part('template-parts/brand-partner/program-content'); ?>
-        </div>
-      </article>
-    <?php endwhile; ?>
-  </div>
-</main>
+  <?php while (have_posts()) : the_post(); ?>
+    <?php get_template_part('template-parts/brand-partner/program-content'); ?>
+  <?php endwhile; ?>
 
 <?php get_footer(); ?>
